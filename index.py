fe3b23e6f4d3eed058a9e4d3fcaac02af36de187
sarbo-sarcar <23f2002621@ds.study.iitm.ac.in>
2025-01-10 00:15:18 +0530

    Create index.py

---
+++ index.py
@@ -0,0 +1,22 @@
+import json
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+import uvicorn
+
+app = FastAPI()
+
+@app.get("/api")
+async def get_marks(request: Request):
+    names = request.query_params.getlist("name")
+    with open("q-vercel-python.json", "r") as file:
+        data = json.load(file)
+    m1 = m2 = 0
+    for i in data:
+        if i["name"]==names[0]:
+            m1 = i["marks"]
+        elif i["name"]==names[1]:
+            m2 = i["marks"]
+    return JSONResponse(content={"marks": [m1, m2]})
+
+if __name__=="__main__":
+    uvicorn.run(app)
